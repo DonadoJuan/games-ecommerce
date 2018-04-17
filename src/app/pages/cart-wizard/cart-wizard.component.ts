2441502c0e46da88010f84354307586346116559
Unknown <donado.juan@gmail.com>
2018-04-17 08:52:51 -0300

    Carrrito de compra paso 1

--- src/app/pages/cart-wizard/cart-wizard.component.ts
+++ src/app/pages/cart-wizard/cart-wizard.component.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartWizardComponent implements OnInit {
 
+  settings: any;
+  data: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.settings = {
+      actions: false,
+      hideSubHeader: true,
+      columns: {
+        image: {
+          title: 'IMAGEN',
+          type: 'html'
+        },
+        product: {
+          title: 'PRODUCTO'
+        },
+        quantity: {
+          title: 'CANTIDAD'
+        },
+        retailPrice:{
+          title: '$ UNITARIO'
+        },
+        subtotal:{
+          title: '$ SUBTOTAL'
+        }
+      },
+      defaultStyle: true,
+      attr: {
+        class: 'table'
+      }
+    }
+
+    this.data = [
+      {
+        product: 'SOUL CALIBUR VI',
+        image: '<img src="../../../../assets/slider/gd-calibur.jpg" class="align-center" width="112px" height="130px"/>',
+        quantity: "1",
+        retailPrice: "$2000",
+        subtotal:"$2000"
+      },
+      {
+        product: 'MARIO ODYSSEY',
+        image: '<img src="../../../../assets/slider/gd-gta.jpg" class="align-center" width="112px" height="130px"/>',
+        quantity: "1",
+        retailPrice: "$2000",
+        subtotal:"$2000"
+      },
+      {
+        product: 'FAR CRY 5',
+        image: '<img src="../../../../assets/slider/gd-farcry5.jpg" class="align-center" width="112px" height="130px"/>',
+        quantity: "1",
+        retailPrice: "$2000",
+        subtotal:"$2000"
+      }
+    ]
   }
 
 }
